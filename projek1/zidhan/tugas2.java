bcb8c8b2092c72579cd9b09c02e3f7cdc06dde38
Zidhan Arzaaq Karim <152593050+zidhannnn1@users.noreply.github.com>
2024-08-26 09:36:34 +0000

    week2

---
+++ projek1/zidhan/tugas2.java
@@ -0,0 +1,14 @@
+public class tugas2 {
+    public static void main(String[] args) {
+        String nama = "Zidhan Arzaaq Karim";
+        String npm = "2340506063";
+        String alamat = "Temanggung";
+        String no_hp = "085704988928";
+
+        System.out.println("Biodata :");
+        System.err.println("Nama      : " + nama);
+        System.err.println("NPM       : " + npm);
+        System.err.println("Alamat    : " + alamat);
+        System.err.println("NO HP     : " + nama);
+    }
+}
